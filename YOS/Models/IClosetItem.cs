acf7036f4b6f669ee12974103df9675b3f90648e
TimmyChannel <timmykanal@gmail.com>
2023-05-20 22:37:21 +0300

    Created Interfaces and Enums

---
+++ YOS/Models/IClosetItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YOS.Models
+{
+    public interface IClosetItem
+    {
+        public string Name { get; set; }
+
+    }
+}
