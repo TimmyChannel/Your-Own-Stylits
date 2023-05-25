8f6ee01e0c7ab711dfda60677cd31842a0ad7b7a
TimmyChannel <timmykanal@gmail.com>
2023-05-25 14:27:49 +0300

    Smth

--- YOS/Models/Settings/ViewPortSettings.cs
+++ YOS/Models/Settings/ViewPortSettings.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HelixToolkit.Wpf.SharpDX;
+using YOS.Models.Mannequin;
 
 namespace YOS.Models.Settings
 {
@@ -11,11 +12,20 @@ namespace YOS.Models.Settings
     {
         private List<GroupElement3D> _lightPresets;
         private byte _indexOfCurrentLightPreset;
+        private MannequinSettings _mannequinSettings;
         public GroupElement3D CurrentLightPreset
         {
             get => _lightPresets[_indexOfCurrentLightPreset];            
         }
+        public List<GroupElement3D> LightPresetList
+        {
+            get => _lightPresets;
+        }
         public Viewport3DX Viewport { get; init; }
-
+        public EnvironmentMap3D Environment { get; init; }
+        public ViewPortSettings()
+        {
+            
+        }
     }
 }
