edc13b56fd70ff65c6757f043adcea6b92ea8a8d
DXN41K <daniil.solowyow2003@gmail.com>
2023-05-25 11:31:18 +0300

    start dev selectcloth algoruthm

---
+++ YOS/Models/SelectClozzes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YOS.Models.EnumParams;
+
+namespace YOS.Models
+{
+    static class SelectClozzes
+    {
+        public static List<Item> Algorithm()
+        {
+            
+            var list = new List<Item>();
+            
+
+            
+            return list;
+
+        }
+    }
+}
