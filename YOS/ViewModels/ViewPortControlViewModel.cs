e8138172d32bdb59a5a4b98d6096098ae09bf44d
TimmyChannel <timmykanal@gmail.com>
2023-05-28 12:55:23 +0300

    Working with loading mtl

--- YOS/ViewModels/ViewPortControlViewModel.cs
+++ YOS/ViewModels/ViewPortControlViewModel.cs
@@ -16,6 +16,9 @@ using YOS.Models;
 using YOS.Models.EnumParams;
 using System.Diagnostics;
 using HelixToolkit.Wpf.SharpDX.Model;
+using SharpDX.Toolkit.Graphics;
+using SharpDX.Direct3D11;
+using System.Threading;
 
 namespace YOS.ViewModels
 {
@@ -24,6 +27,9 @@ namespace YOS.ViewModels
         public event PropertyChangedEventHandler? PropertyChanged;
         private void OnPropertyChanged([CallerMemberName] string propertyName = "", bool allProperties = false) =>
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(allProperties ? null : propertyName));
+        public ObservableElement3DCollection MannequinModel { get; } = new ObservableElement3DCollection();
+        private SynchronizationContext context = SynchronizationContext.Current;
+
         public Geometry3D Geometry { private set; get; }
         public PBRMaterial Material { private set; get; }
         public Camera Camera { private set; get; }
@@ -37,28 +43,78 @@ namespace YOS.ViewModels
             };
             var resourcePath = $"{AppDomain.CurrentDomain.BaseDirectory}Resources\\";
             var reader = new ObjReader();
-            
-            var models = reader.Read($"{resourcePath}Tshirt.obj");
-            Geometry = models[0].Geometry;
-            for (int i = 0; i < Geometry.Positions.Count; i++)
+            //var models = reader.Read($"{resourcePath}Avatars\\Female\\Models\\APose.obj");
+            LoadObj($"{resourcePath}Avatars\\Female\\Models\\APose.obj");
+        }
+        public void LoadObj(string path)
+        {
+            var reader = new ObjReader();
+            var objCol = reader.Read(path);
+            AttachModelList(objCol);
+        }
+
+        public void AttachModelList(List<Object3D> objs)
+        {
+            for (int i = 0; i < objs.Count; ++i)
             {
-                Geometry.Positions[i] += new Vector3(0, -40, 0);
+                var ob = objs[i];
+                var vertColor = new Color4((float)i / objs.Count, 0, 1 - (float)i / objs.Count, 1);
+                ob.Geometry.Colors = new Color4Collection(Enumerable.Repeat(vertColor, ob.Geometry.Positions.Count));
+                ob.Geometry.UpdateOctree();
+                ob.Geometry.UpdateBounds();
+
+                context.Post((o) =>
+                {
+                    var scaleTransform = new Media3D.ScaleTransform3D(1, 1, 1);
+                    var s = new MeshGeometryModel3D
+                    {
+                        Geometry = ob.Geometry,
+                        CullMode = SharpDX.Direct3D11.CullMode.Back,
+                        IsThrowingShadow = true,
+                        Transform = scaleTransform,
+                    };
+
+                    var diffuseMaterial = new DiffuseMaterial();
+                    PBRMaterial pbrMaterial = null;
+                    if (ob.Material is PhongMaterialCore p)
+                    {
+                        var phong = p.ConvertToPhongMaterial();
+                        phong.RenderEnvironmentMap = true;
+                        phong.RenderShadowMap = true;
+                        phong.RenderSpecularColorMap = false;
+                        s.Material = phong;
+                        diffuseMaterial.DiffuseColor = p.DiffuseColor;
+                        diffuseMaterial.DiffuseMap = p.DiffuseMap;
+                        pbrMaterial = new PBRMaterial()
+                        {
+                            AlbedoColor = p.DiffuseColor,
+                            AlbedoMap = p.DiffuseMap,
+                            NormalMap = p.NormalMap,
+                            RoughnessMetallicMap = p.SpecularColorMap,
+                            AmbientOcculsionMap = p.SpecularColorMap,
+                            RenderShadowMap = true,
+                            RenderEnvironmentMap = true,
+                            MetallicFactor = 1, // Set to 1 if using RMA Map
+                            RoughnessFactor = 1 // Set to 1 if using RMA Map
+                        };
+                    }
+
+                    //if (ob.Transform != null && ob.Transform.Count > 0)
+                    //{
+                    //    s.Instances = ob.Transform;
+                    //}
+                    this.MannequinModel.Add(new MeshGeometryModel3D
+                    {
+                        Geometry = ob.Geometry,
+                        CullMode = SharpDX.Direct3D11.CullMode.Back,
+                        IsThrowingShadow = true,
+                        Transform = scaleTransform,
+                        Material = pbrMaterial
+                    }); ;
+                    
+
+                }, null);
             }
-            var tshirtuv = new UVTransform();
-            tshirtuv.Scaling = new Vector2(10F, 10F);
-            var material = new PBRMaterial
-            {
-                RenderAlbedoMap = true,
-                RenderAmbientOcclusionMap = true,
-                RenderNormalMap = true,
-                AmbientOcculsionMap = TextureModel.Create($"{resourcePath}Textures\\GrayFabricTex\\ambientocclusion.png"),
-                NormalMap = TextureModel.Create($"{resourcePath}Textures\\GrayFabricTex\\normal.png"),
-                RoughnessMetallicMap = TextureModel.Create($"{resourcePath}Textures\\GrayFabricTex\\roughness.png"),
-                AlbedoMap = TextureModel.Create($"{resourcePath}Textures\\GrayFabricTex\\Tshirt.png"),
-                RoughnessFactor = 0.2f,
-                AlbedoColor = new Color4(1, 0.1f, 0.2f, 1)
-            };
-            Material = material;
         }
     }
 }
