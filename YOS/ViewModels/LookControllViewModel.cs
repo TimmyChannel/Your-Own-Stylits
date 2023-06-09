72d4d5e56191e811bf49b0f23b054d841f055f67
DXN41K <daniil.solowyow2003@gmail.com>
2023-06-09 20:23:55 +0300

    some edits

---
+++ YOS/ViewModels/LookControllViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+using YOS.Models.EnumParams;
+using YOS.Models.Mannequin;
+
+namespace YOS.ViewModels
+{
+    class LookControllViewModel
+    {
+        private ICommand _generateLook;
+        public ICommand GenerateLook => _generateLook ??= new RelayCommand(OnLookButtonPressed);
+        private void OnLookButtonPressed()
+        {
+        //    if (_currentModelMaterial != null)
+        //    {
+        //        if (!Materials.Contains(SelectedMaterial))
+        //            SelectedMaterial = Materials[0];
+        //        _mannequinSettings.SetMaterialToSelectedItem(SelectedMaterial, CurrentModelMaterial.AlbedoColor);
+        //        Debug.WriteLine("Material was applied");
+        //    }
+        }
+    }
+}
