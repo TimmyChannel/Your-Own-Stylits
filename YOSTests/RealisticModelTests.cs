95f70522fd13a3d45c8aa57a6424ddaed245b46c
TimmyChannel <timmykanal@gmail.com>
2023-06-14 14:03:33 +0300

    Added Testing Proj

---
+++ YOSTests/RealisticModelTests.cs
@@ -0,0 +1,12 @@
+using YOS.Models.Mannequin;
+namespace YOSTests;
+
+[TestClass]
+public class RealisticModelTests
+{
+    [TestMethod]
+    public void CreateRealisticMannequinMoodelTest()
+    {
+
+    }
+}
